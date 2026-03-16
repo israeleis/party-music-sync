f023253b1d0c1653f23c7b733106104f73b16cf4
Israel Eisenman <israeleis@gmail.com>
2026-03-16 22:42:10 +0200

    deploy: publish web app to GitHub Pages

--- packages/core/src/index.ts
+++
@@ -1,6 +0,0 @@
-export type { ColorState, WsMessage } from './types';
-export type { BandResult } from './BandProcessor';
-export { BandProcessor } from './BandProcessor';
-export { ColorEngine } from './ColorEngine';
-export { SyncManager } from './SyncManager';
-export type { SyncManagerOptions, SyncRole } from './SyncManager';
--- packages/mobile/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!.*(react-native|@react-native|expo|@expo|@shopify/react-native-skia|@shopify\\+react-native-skia|react-navigation|@react-navigation|@testing-library/react-native|react-native-safe-area-context))',
-  ],
-  moduleNameMapper: {
-    '^@partylight/core$': '<rootDir>/../core/src/index.ts',
-  },
-};
